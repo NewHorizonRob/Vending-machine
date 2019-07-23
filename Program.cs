592333ed1a973d97117be385dd3d60267e352589
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-23 18:05:53 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch4_Vending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VendingMachine blah;
+            blah = new VendingMachine();
+
+            blah.DepositCoin(25);
+            blah.DepositCoin(25);
+            blah.DepositCoin(10);
+            blah.DepositCoin(50);
+            blah.DepositCoin(100);
+            blah.GetDrink();
+            blah.DepositCoin(25);
+            blah.DepositCoin(25);
+            blah.DepositCoin(10);
+            blah.GetDrink();
+            blah.GetRefund();
+            blah.DepositCoin(100);
+            blah.DepositCoin(25);
+            blah.GetDrink();
+            blah.GetRefund();
+            Console.WriteLine("PAK");
+            Console.ReadKey();
+        }
+    }
+}
